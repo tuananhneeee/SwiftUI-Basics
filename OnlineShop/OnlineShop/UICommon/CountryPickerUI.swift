2036a0c666bc0f711f0832d003980d801d488759
tuan-anh-mk1 <trananh16112001@gmail.com>
2024-04-08 21:46:58 +0700

    [A] adding social connect button, adding country picker when entering phone number, complete the UI of the SignInView . ..

---
+++ OnlineShop/OnlineShop/UICommon/CountryPickerUI.swift
@@ -0,0 +1,47 @@
+//
+//  CountryPickerUI.swift
+//  OnlineShop
+//
+//  Created by tuan anh on 8/4/24.
+//
+
+import SwiftUI
+import CountryPicker
+
+struct CountryPickerUI: UIViewControllerRepresentable {
+    
+    @Binding var country: Country?
+    
+    class Coordinator: NSObject, CountryPickerDelegate{
+        var parent: CountryPickerUI
+        
+        init(_ parents: CountryPickerUI) {
+            self.parent = parents
+        }
+        
+        func countryPicker(didSelect country: Country) {
+            parent.country = country
+        }
+    }
+    
+    
+    func makeUIViewController(context: Context) -> some CountryPickerViewController {
+        
+        let countryPicker = CountryPickerViewController()
+        countryPicker.selectedCountry = "VN"
+        countryPicker.delegate = context.coordinator
+        
+        return countryPicker
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+
+    }
+    
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    
+}
--- OnlineShop/OnlineShop/View/Login/SignInVIew.swift
+++ OnlineShop/OnlineShop/View/Login/SignInVIew.swift
@@ -6,10 +6,13 @@
 //
 
 import SwiftUI
+import CountryPicker
 
 struct SignInVIew: View {
     
     @State var txtMobile: String = ""
+    @State var isShowPicker: Bool = false
+    @State var countryObj: Country?
     
     var body: some View {
         ZStack (alignment: .top) {
@@ -29,21 +32,28 @@ struct SignInVIew: View {
             }
             
             ScrollView {
-                VStack {
-                    Text("Get to know more about doing an mobile app using SwiftUI")
-                        .font(.customfont(.bold, fontSize: 28))
+                VStack (alignment: .leading) {
+                    Text("Get to know more about using SwiftUI")
+                        .font(.customfont(.bold, fontSize: 26))
                         .foregroundColor(.primaryText)
                         .multilineTextAlignment(.leading)
-                        .padding(.bottom, 25)
+                        .padding(.bottom, 10)
+                    
                     HStack {
                         Button {
-                        
+                            isShowPicker = true
                         } label: {
-                            Image("")
+//                            Image("")
                             
-                            Text("+84")
-                                .font(.customfont(.regular, fontSize: 18))
-                                .foregroundStyle(Color.primaryText)
+                            if let countryObj = countryObj {
+                                Text("\(countryObj.isoCode.getFlag())")
+                                    .font(.customfont(.regular, fontSize: 26))
+                                
+                                Text("+\(countryObj.phoneCode)")
+                                    .font(.customfont(.regular, fontSize: 18))
+                                    .foregroundStyle(Color.primaryText)
+                            }
+
                         }
                         
                         TextField("Enter your number:", text: $txtMobile)
@@ -53,20 +63,61 @@ struct SignInVIew: View {
                     
                     
                     Divider()
-                        .padding(.bottom)
-                    
-                    Text("Get to know more about doing an mobile app using SwiftUI")
-                        .font(.customfont(.bold, fontSize: 28))
-                        .foregroundColor(.primaryText)
-                        .multilineTextAlignment(.leading)
                         .padding(.bottom, 25)
+                    
+                    Text("Or connect with social media")
+                        .font(.customfont(.semibold, fontSize: 14))
+                        .foregroundColor(.textTile)
+                        .multilineTextAlignment(.center)
+                        .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                        .padding(.bottom, 25)
+                    
+                    Button {
+                    } label: {
+                        Image("google")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        Text("Continue with Google")
+                            .font(.customfont(.regular, fontSize: 18))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+
+                    }
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 60, maxHeight: 60)
+                    .background(Color(hex: "5383EC"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20,style: .continuous))
+                    .padding(.bottom, 8)
+
+                    
+                    Button {
+                    } label: {
+                        Image("fb")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 20, height: 20)
+                        Text("Continue with Facebook")
+                            .font(.customfont(.regular, fontSize: 18))
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.center)
+
+                    }
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/, minHeight: 60, maxHeight: 60)
+                    .background(Color(hex: "4A66AC"))
+                    .clipShape(RoundedRectangle(cornerRadius: 20,style: .continuous))
 
                 }
                 .padding(.horizontal, 20)
                 .frame(width: .screenWidth, alignment: .leading)
-                .padding(.top, .topInsets + .screenWidth * 0.7)
+                .padding(.top, .topInsets + .screenWidth )
             }
         }
+        .onAppear{
+            self.countryObj = Country(phoneCode: "84", isoCode: "VN")
+        }
+        .sheet(isPresented: $isShowPicker, content: {
+            CountryPickerUI(country: $countryObj)
+        })
         .navigationTitle("")
         .navigationBarBackButtonHidden(true)
         .navigationBarHidden(true)
